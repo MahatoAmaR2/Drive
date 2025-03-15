e268589ba44919d7786d9cc238e6fe4335759919
MahatoAmaR2 <bauaa476@gmail.com>
2025-03-15 15:14:51 +0530

    Folder and File initialized

---
+++ Frontend/Drive/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1 className='text-7xl underline'>Har Har Mahadev</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
